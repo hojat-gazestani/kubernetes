bf99a24ee18abc41a94fec066b75656fd1b04573
hojat-gazestani <Hojat.Gazestani@gmail.com>
2023-06-17 11:23:14 +0330

    12

---
+++ Kubeadm/scripts/06-intstall-containerd.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install required packages
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates lsb-release
+
+# Add Docker repo
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Install containerd
+sudo apt update
+sudo apt install -y containerd.io
+
+# Configure containerd and start service
+sudo su -
+mkdir -p /etc/containerd
+containerd config default>/etc/containerd/config.toml
+
+# restart containerd
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+systemctl status containerd
